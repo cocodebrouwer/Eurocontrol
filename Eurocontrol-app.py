21e8856cc78281572de83061680313e995f0f015
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-12-10 11:17:49 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -3,7 +3,6 @@ import streamlit as st
 
 #Importeer de benodigde packages
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
@@ -11,15 +10,14 @@ import statsmodels.api as sm
 import seaborn as sns
 
 #Titel toevoegen
-st.title("✈️ NL luchthavens en COVID-19 ✈️")
+st.title("Eurocontrol Dashboard")
 
 #Tekst toevoegen
 st.markdown("""
 Welkom bij ons Dashboard over de Nederlandse Luchthavens in tijden van COVID-19!\n
 \n
 Wij zijn Coco de Brouwer en Inge Vijsma, derdejaars Aviation studenten en volgen momenteel de minor Data Science. 
-Voor de eindpresentatie van het vak Visual Analytics hebben wij een Dashboard gemaakt over de bedrijvigheid van de Nederlandse Luchthavens tijdens de pandemie. 
-Het Dashboard bestaat uit vijf verschillende soorten grafieken, deze zijn te selecteren in het keuzemenu.  
+
 \n
 Veel plezier met het bekijken van ons interactieve Dashboard en het uitproberen van de verschillende keuzemogelijkheden!
 \n
