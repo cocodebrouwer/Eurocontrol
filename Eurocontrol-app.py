27f9b0d6ea3bc4b9538981dc3b819c25f2fefa5b
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-14 13:01:56 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 # import seaborn as sns
 
 
-st.beta_set_page_config(layout="centered")
+st.beta_set_page_config(layout="wide")
 # Data inladen
 Data = pd.read_csv('FINAL.csv')
 
