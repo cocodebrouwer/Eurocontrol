27214f977fffb405af909f839b8112969f8e974f
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-14 13:52:00 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -33,7 +33,9 @@ To = col2.selectbox(label= 'To', options= Data['To'].unique())
 
 # Dagen
 dagen = Data[Data['To']==To]['Weekday'].sort_values('Weekday number')['Weekday'].unique()
-test = col3.radio(label= 'Dagen', options= dagen)
+st.dataframe(dagen)
+
+# test = col3.radio(label= 'Dagen', options= dagen)
 
 
 # with st.expander('Meer informatie:'):
