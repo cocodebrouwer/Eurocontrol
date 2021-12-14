797ae79c4d7a4c6bcbd0d1adfdf520f7568d1cb5
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-14 13:35:45 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -33,8 +33,8 @@ To = col2.selectbox(label= 'To', options= Data['To'].unique())
 
 # Dagen
 dagen = Data[Data['To']==To]['Weekday']
-Weekdays = {'Monday':0, 'Tuesday':1, 'Wednesday':2, 'Thursday':3, 'Friday':4, 'Saturday':5, 'Sunday':6}
-dagen.sort_values(by=['Weekday'], key=lambda x: x.map(Weekdays))
+dagen['Weekday'] = pd.Categorical(dagen['Weekday'], ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+dagen.sort_values('Weekday')
 test = col3.radio(label= 'Dagen', options= dagen.unique())
 
 
