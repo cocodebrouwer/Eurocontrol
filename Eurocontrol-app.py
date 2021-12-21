4113aa7b22a3c7093e77de333f97695a2b53ce9c
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-21 20:30:12 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -144,7 +144,7 @@ if nav == "Passengers":
 elif nav == "Governments":
   st.title("Eurocontrol Dashboard for gevernments")
   
-  Airlines_compact = Data.drop_duplicates(subset=['Airline', 'Keurmerk'])
+  Airlines_compact = Data.drop_duplicates(subset=['Airline', 'Keurmerk']).sort_values('Mean CO2 per flight per airline (kg)')
   
   col1, col2 = st.columns(2)
   
