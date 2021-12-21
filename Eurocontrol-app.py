6649fe53cf393b6b84d5981bdb34787f14afcbdd
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-21 11:18:55 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -6,10 +6,10 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from streamlit_folium import folium_static
+# from streamlit_folium import folium_static
 import folium
 # import statsmodels.api as sm
-import seaborn as sns
+# import seaborn as sns
 
 
 st.set_page_config(layout="wide")
