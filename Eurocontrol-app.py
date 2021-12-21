0029707c1910e2d9b605f977616ba35c5c68e72d
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-21 13:14:21 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -43,9 +43,10 @@ st.title("Eurocontrol Dashboard")
 
 col1, col2, col3 = st.columns((3,3,2))
 
+From = st.write('**From**')
 
 # From
-From = col1.selectbox(label= '**From**', options= Data['From'].unique())
+From = col1.selectbox(label= From, options= Data['From'].unique())
 
 # To
 To = col2.selectbox(label= '**To**', options= Data['To'].unique())
