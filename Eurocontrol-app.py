f3543d8b6957ed4c4cc27b0242dc29e13ae04f12
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-21 20:15:29 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -150,7 +150,7 @@ elif nav == "Governments":
                    
   Keurmerk = col2.multiselect(label='Qualitymark', options=Data['Keurmerk'].unique())
   
-  Airlines = Data[Data['Airline'] isin Airline]
+  Airlines = Data[Data['Airline'] isin(Airline)]
                               
   
   st.write(Airlines)
