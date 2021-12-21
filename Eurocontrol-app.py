20c0380fd700d900ee4693cae2c479a064049bf1
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-21 14:18:27 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -89,7 +89,7 @@ if nav == "Passengers":
   col8.write(str(int(vluchten_sorted['Mean distance (km)'].mean())) + ' km')
   col8.write(str(int(vluchten_sorted['Flight time (min)'].mean())) + ' min')
 
-  col1, col2, col3 = st.columns((4,1,1)
+  col1, col2, col3 = st.columns((4,1,1))
   
   # Begin Map
   DataDone = pd.read_csv('Datadone.csv', index_col='To')
