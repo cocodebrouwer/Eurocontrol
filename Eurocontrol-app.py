7bf4081597bbe3c246cb730723b69a00eca93364
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2021-12-22 12:39:14 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -201,5 +201,5 @@ elif nav == "Airlines":
   
   st.write(Data)
   
-  loadfactor1 = col1.slider(label='Loadfactor 1', min_value=0, max_value=1, value=)
-  loadfactor2 = col2.slider(label='Loadfactor 2', min_value=0, max_value=1, )
+  loadfactor1 = col1.slider(label='Loadfactor 1', min_value=0, max_value=1)
+  loadfactor2 = col2.slider(label='Loadfactor 2', min_value=0, max_value=1)
