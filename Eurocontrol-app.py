d64bdc066bf77ad4185211a6c2dd40d1c33052c8
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2022-01-10 14:55:57 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -206,11 +206,16 @@ elif nav == "Airlines":
   loadfactor1 = col1.slider(label='Loadfactor 1', min_value=0.0, max_value=1.0, value=loadfactor1, step=0.01)
   loadfactor2 = col2.slider(label='Loadfactor 2', min_value=0.0, max_value=1.0, value=loadfactor2, step=0.01)
   
-  st.write(Data3)
+#   st.write(Data3)
   
-  Data3['CO2 loadfactor'] = Data3['CO2 per seat per airline (kg/km)'] / Data3['Loadfactor']
+#   Data3['CO2 loadfactor'] = Data3['CO2 per seat per airline (kg/km)'] / Data3['Loadfactor']
+#   Data3.loc[CO2 loadfactor['Mean CO2 per seat per airline (kg/km)'] <= 0.065, 'Keurmerk'] = 'A'
+#   flights_seats_distance_fuel_range.loc[((flights_seats_distance_fuel_range['Mean CO2 per seat per airline (kg/km)'] > 0.065) & (flights_seats_distance_fuel_range['Mean CO2 per seat per airline (kg/km)'] <= 0.075)), 'Keurmerk'] = 'B'
+#   flights_seats_distance_fuel_range.loc[((flights_seats_distance_fuel_range['Mean CO2 per seat per airline (kg/km)'] > 0.075) & (flights_seats_distance_fuel_range['Mean CO2 per seat per airline (kg/km)'] <= 0.085)), 'Keurmerk'] = 'C'
+#   flights_seats_distance_fuel_range.loc[((flights_seats_distance_fuel_range['Mean CO2 per seat per airline (kg/km)'] > 0.085) & (flights_seats_distance_fuel_range['Mean CO2 per seat per airline (kg/km)'] <= 0.095)), 'Keurmerk'] = 'D'
+#   flights_seats_distance_fuel_range.loc[flights_seats_distance_fuel_range['Mean CO2 per seat per airline (kg/km)'] > 0.095, 'Keurmerk'] = 'E'
   
-  st.write(Data3)
+#   st.write(Data3)
   
   
   col1, col2, col3, col4 = st.columns(4)
