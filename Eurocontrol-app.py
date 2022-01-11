bd1528fe51db412c8acf9fadf23acb22359e1f88
ingevijsma <91197377+ingevijsma@users.noreply.github.com>
2022-01-11 12:16:44 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -110,7 +110,7 @@ if nav == "Passengers":
   with col1.expander('More information:'):
         st.markdown("""**Airline:** This includes 34 airlines flying on the AMS-network.\n
   **Quality mark:** This provides information about the average CO2 emissions per seat (in kg/km) of an airline. This is then divided into 5 categories, shown below.\n
-  A. <= 0.065 kg CO2 per seat km\n
+  A. ≤ 0.065 kg CO2 per seat km\n
   B. 0.065 - 0.075 kg CO2 per seat km\n
   C. 0.075 - 0.085 kg CO2 per seat km\n
   D. 0.085 - 0.095 kg CO2 per seat km\n
