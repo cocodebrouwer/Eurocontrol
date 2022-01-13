dd3d9692f8741abb0eaa2e02e5f1fa2231e85a1e
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2022-01-13 12:39:57 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -318,7 +318,7 @@ elif st.session_state.page == 2:
 	
 
   Government = Data2.to_csv('Government.csv')
-  st.write(type(Government)
+  col1.write(type(Government)
   #st.download_button(label='Government', data=Government)
   
   #Add a black line
