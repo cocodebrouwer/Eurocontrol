17b51beb39078597020a1a1986c745c7af608098
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2022-01-18 11:33:16 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -308,7 +308,9 @@ elif st.session_state.page == 2:
 	
 	Merged.rename(columns = {'Unnamed: 0':'Ranking'}, inplace = True)
 	Merged['Ranking'] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
-	st.write(Merged)
+	Merged.to_csv('Government.csv')
+	
+	st.download_button(label='Download', data=csv, file_name='Government.csv', mime='text/csv')
 	
 
 	#Add a black line
