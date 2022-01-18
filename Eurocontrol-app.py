eee92f88e461869d20a5c415b40e0e8bae6cdb29
TimonvanLeeuwen <91192197+TimonvanLeeuwen@users.noreply.github.com>
2022-01-18 10:35:40 +0100

    Update Eurocontrol-app.py

--- Eurocontrol-app.py
+++ Eurocontrol-app.py
@@ -307,7 +307,7 @@ elif st.session_state.page == 2:
   E. > 0.095 CO2 per seat (kg/km)\n
   **CO2 (kg/km):** This provides information about the average CO2 emissions per seat (in kg/km) of an airline.""")
 			
-	st.write('test')
+st.write('test')
 	
 
   #Add a black line
